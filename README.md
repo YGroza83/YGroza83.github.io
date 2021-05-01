a1013ee85d8100c70f0848b29af0c267b0c93109
YGroza <ygroza83@gmail.com>
2021-05-01 11:49:27 -0400

    Finalized upload

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# YGroza83.github.io
+# WorldHappinessViz
+
+## Team Members:
+* Kasey Wilson
+* Yuri Groza
+* Brad Medford
+* Kelly Rose 
+
+## Project Description/Outline:
+Your task is to tell a story with Data Visualization 
+* Visualization must include a Python Flask-powered API, HTML/CSS, JavaScript, and at least one database (SQL, MongoDB, SQLite, etc.)
+* Your project should fall into one of the below four tracks:
+* Include at least one JS library
+* Powered by a dataset with at least 100 records
+* Include some level of user-driven interaction
+* Final visualization will have at least three views
+
+## Datasets:
+Use the following dataset related to 2021 World Happiness, procured from Kaggle. Includes CSV file. 
+* https://www.kaggle.com/ajaypalsinghlo/world-happiness-report-2021
+
+## Rough Breakdown of Tasks
+* Import CSV into Python to generate flask-powered API
+* Create SQL database and load data 
+* Create HTML and CSS that will display a dashboard page with multiple charts that update from our dataset
+* Create Heatmap with JS Heatmap library
+* Create interactive dropdowns or input box to select different happiness factors (i.e. life expectancy) vs. happiness score and chart those on a scatter plot. 
+* Create bar chart with top 10 countries for a third visualization. 
---
+++ static/js/logic.js
@@ -0,0 +1,55 @@
+url = "https://cors-anywhere.herokuapp.com/https://limitless-brushlands-31442.herokuapp.com"
+
+// url = "https://ygroza.com/results.json"
+
+var light = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "light-v10",
+  accessToken: API_KEY
+});
+
+var dark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "dark-v10",
+  accessToken: API_KEY
+});
+
+var street = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+});
+
+var myMap = L.map("map", {
+  center: [0, 0],
+  zoom: 2,
+  layers: street
+});
+
+var baseMaps = {
+  Light: light,
+  Dark: dark,
+  Street: street
+};
+
+d3.json(url).then(function (data) {
+  var marker_list = [];
+  
+  data.forEach(({ Lat, Long, Name, Score }) => {
+    marker_list.push(L.marker([Lat,Long]).bindPopup("<h3>" + Name + "</h3><hr><p>"  + "Happiness Score: " + Score + "</p"))
+  });
+  
+  var country_list = L.layerGroup(marker_list).addTo(myMap);
+  
+  var overlayMaps = {
+    Countries: country_list
+  };
+  
+  L.control.layers(baseMaps,overlayMaps).addTo(myMap);
+});
+
